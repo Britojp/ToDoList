e54279cbd37c2be8014d3d8d1c2cfa70e0c23f81
Britojp <joaopedrodebritotome@discente.ufg.br>
2025-01-27 14:30:36 -0300

    implementação dos comparadores e menus

---
+++ src/ComparadorCategoria.java
@@ -0,0 +1,12 @@
+package comparador;
+
+
+import java.util.Comparator;
+
+class ComparadorCategoria implements Comparator<Tarefa> {
+
+    @Override
+    public int compare(Tarefa tarefa1, Tarefa tarefa2) {
+        return tarefa1.getCategoria().compareTo(tarefa2.getCategoria());
+    }
+}
---
+++ src/ComparadorStatus.java
@@ -0,0 +1,25 @@
+package comparador;
+
+import java.util.Comparator;
+
+
+class ComparadorStatus implements Comparator<Tarefa> {
+
+    @Override
+    public int compare(Tarefa tarefa1, Tarefa tarefa2) {
+        return Integer.compare(obterOrdemStatus(tarefa1.getStatus()), obterOrdemStatus(tarefa2.getStatus()));
+    }
+
+    private int obterOrdemStatus(String status) {
+        switch (status) {
+            case "todo":
+                return 1;
+            case "doing":
+                return 2;
+            case "done":
+                return 3;
+            default:
+                return 4;
+        }
+    }
+}
